9c6ab8a624266628834d3f243e46bc39fbadf401
Jakub Jankowski <jakubj31@gmail.com>
2022-05-10 13:01:39 +0200

    Simple next.js app with fetching data from API

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Duis consectetur reprehenderit pariatur est elit reprehenderit magna et. Commodo minim et aliquip qui ut nisi sit magna labore exercitation cupidatat labore consectetur. Lorem eiusmod cillum Lorem sunt laborum labore cillum consectetur magna deserunt ex. Anim cillum deserunt irure tempor commodo. Eiusmod eu ipsum deserunt commodo mollit fugiat ullamco fugiat consequat exercitation esse voluptate tempor id. Mollit irure aliqua sit nostrud elit. Est labore laborum sunt ex tempor in.</p>
+                <p>Duis consectetur reprehenderit pariatur est elit reprehenderit magna et. Commodo minim et aliquip qui ut nisi sit magna labore exercitation cupidatat labore consectetur. Lorem eiusmod cillum Lorem sunt laborum labore cillum consectetur magna deserunt ex. Anim cillum deserunt irure tempor commodo. Eiusmod eu ipsum deserunt commodo mollit fugiat ullamco fugiat consequat exercitation esse voluptate tempor id. Mollit irure aliqua sit nostrud elit. Est labore laborum sunt ex tempor in.</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
